c1e56e5f8dcfc18837dc789cdfbee0256c68b66e
Lintangms14 <merayusukmalintang@gmail.com>
2024-10-18 00:57:13 +0700

    untuk memasukkan gambar logo

--- app/src/main/java/com/example/tugasbasiccomposse/MainActivity.kt
+++ app/src/main/java/com/example/tugasbasiccomposse/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -15,9 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tugasbasiccomposse.ui.theme.TugasBasicComposseTheme
 
@@ -59,6 +63,11 @@ fun TugasBasicComposse(lintang : Modifier = Modifier){
                 fontStyle = FontStyle.Italic,
                 fontWeight = FontWeight.Bold
         )
+        (
+                Image(painter = painterResource(id = R.drawable.umy),
+                    contentDescription = null,
+
+                ))
     }
 }
 
